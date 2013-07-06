7f3af925d4b97008f9ce75c58c73ed015dcf0acc
Steve Wilson <thesawolf@gmail.com>
2013-07-06 08:18:26 -0400

    Added SmartAssV2 and MinMax CPU governors
    Change-Id: Ib6dee5da1003132abf61960037135d3194e4fe82

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -361,6 +361,12 @@ extern struct cpufreq_governor cpufreq_gov_conservative;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTERACTIVE)
 extern struct cpufreq_governor cpufreq_gov_interactive;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_interactive)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTASS2)
+extern struct cpufreq_governor cpufreq_gov_smartass2;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_smartass2)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_MINMAX)
+extern struct cpufreq_governor cpufreq_gov_minmax;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_minmax)
 #endif
 
 
